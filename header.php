3d06c254b13de81259ecee6578eec951bdd7e939
UN RA <147945044+Rangnu@users.noreply.github.com>
2024-06-20 15:38:10 +0200

    Update header.php
    deletedforprivacy

--- header.php
+++ header.php
@@ -19,8 +19,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap" rel="stylesheet">
     <!-- Include any additional meta tags or scripts if needed -->
-    <meta name="google-site-verification" content="NLL4VfFoLgDOzdmaoG9kurabCUjcOb7ooycKdsb3AZM" />
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2390907231823910"
+    <meta name="google-site-verification" content="deletedforprivacy" />
+    <script async src="deletedforprivacy"
      crossorigin="anonymous"></script>
      <?php wp_head(); ?>
 </head>
